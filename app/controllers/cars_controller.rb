760b04bf2782f64f3c140a190c6b7651d8e20df2
Alasdair Monk <alasdair.monk@gmail.com>
2013-03-09 01:08:43 +0000

    Fixes

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,19 +1,25 @@
 class CarsController < ApplicationController
   respond_to :json
+  layout :false
+
   def index
     @cars = Car.all
+    respond_with @cars
   end
 
   def show
     @car = Car.find(params[:id])
+    respond_with @car
   end
 
   def new
     @car = Car.new
+    respond_with @car
   end
 
   def edit
     @car = Car.find(params[:id])
+    respond_with @car
   end
 
   def create
